c03a7aec9d8ab3f7912187c5216d2f26f5209766
hugoherculano <hugohercullano@gmail.com>
2023-10-07 12:02:55 -0300

    feat: add create and delete repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: 'test',
+      url: 'http://',
+      techs: 'carros'
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => {
+      return repository.id !== id
+    }))
   }
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(response => {
+        setRepositories(response.data)
+      })
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((item) => (
+          <li key={item.id}>
+            {item.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(item.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
